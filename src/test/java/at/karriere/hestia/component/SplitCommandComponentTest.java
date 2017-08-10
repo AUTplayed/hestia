e2092e4f02e122d00360859ba7b8c9da18dd175a
Philipp Hemmelmayr <philipp.hemmelmayr@karriere.at>
2017-08-10 13:15:48 +0200

    updated splitcommand to handle quotes

--- src/test/java/at/karriere/hestia/component/SplitCommandComponentTest.java
+++ src/test/java/at/karriere/hestia/component/SplitCommandComponentTest.java
@@ -10,7 +10,7 @@ public class SplitCommandComponentTest {
     SplitCommandComponent splitCommandComponent = new SplitCommandComponent();
 
     @Test
-    public void test(){
+    public void test() {
         String commandString = "SET FOO BAR";
 
         CommandContainer commandContainer = splitCommandComponent.split(commandString);
@@ -18,5 +18,15 @@ public class SplitCommandComponentTest {
         assertThat(commandContainer.getArgs().length).as("Check length").isEqualTo(2);
         assertThat(commandContainer.getArgs()[0]).as("Check arg0").isEqualTo("FOO");
         assertThat(commandContainer.getArgs()[1]).as("Check arg1").isEqualTo("BAR");
+    }
+
+    @Test
+    public void testQuotes() {
+        String commandString = "EVAL \"return redis.call('DBSIZE')\" 0";
+        CommandContainer commandContainer = splitCommandComponent.split(commandString);
+        assertThat(commandContainer.getCommand().equals("EVAL"));
+        assertThat(commandContainer.getArgs().length).as("Check length").isEqualTo(2);
+        assertThat(commandContainer.getArgs()[0]).as("Check arg0").isEqualTo("\"return redis.call('DBSIZE')\"");
+        assertThat(commandContainer.getArgs()[1]).as("Check arg1").isEqualTo("0");
     }
 }
