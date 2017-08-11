8b438b9a0f6a271717214323b4e86708106a8c63
Philipp Hemmelmayr <philipp.hemmelmayr@karriere.at>
2017-08-11 10:43:23 +0200

    beautified and commented js code

--- src/main/resources/public/js/hestia-keys.js
+++ src/main/resources/public/js/hestia-keys.js
@@ -5,26 +5,50 @@ var curkey;
 var selectedRow;
 
 $(document).ready(function() {
+
+    //On Search button click
     $("#keys-search").click(function() {
+
+        //get all input values and call getKeys() which, well..., gets the keys
         count = $("#keys-count").val();
         pattern = $("#keys-pattern").val();
+
+        //Reset cursor
         cursor = 0;
         getKeys();
     });
+
+    //On Nextpage button click
     $("#keys-nextpage").click(function() {
-        if(cursor == 0) {
+
+        //When the cursor is 0 we are at the start again -  display "no more pages"
+        if (cursor == 0) {
             $("#keys-error").html("No more pages")
         } else {
+
+            //Else get the next keys
             getKeys();
         }
     });
-    $("#keys-value-delete").click(function () {
-        if(curkey && selectedRow) {
-            var url = "/cli?command=DEL "+curkey+getConnection();
-            $.get(url,function (res) {
-                $("#keys-value-status").html("Deleted: "+ res);
-                if(res > 0) {
+
+    //On Value delete button click
+    $("#keys-value-delete").click(function() {
+
+        //Only if a value is currently selected and displayed
+        if (curkey && selectedRow) {
+
+            //Build url and send delete request to server
+            var url = "/cli?command=DEL " + curkey + getConnection();
+            $.get(url, function(res) {
+
+                //Display deleted amount
+                $("#keys-value-status").html("Deleted: " + res);
+
+                //If there were keys deleted, remove the table row from the UI
+                if (res > 0) {
                     selectedRow.remove();
+
+                    //And clear out the value display
                     $("#keys-value-value").html("");
                 }
             });
@@ -32,38 +56,68 @@ $(document).ready(function() {
     });
 });
 
+/**
+ * Gets the next keys from the db
+ */
 function getKeys() {
+    //Clear all data
     $("#keys-output").html("");
-    $("#keys-error").html("")
+    $("#keys-error").html("");
     $("#keys-value-value").html("");
     $("#keys-value-status").html("");
     curkey = false;
     selectedRow = false;
-    var url = "/keys?cursor="+cursor;
-    if(count && count != "") {
-        url += "&count="+count;
+
+    //Build url depending on filled input forms
+    var url = "/keys?cursor=" + cursor;
+    if (count && count != "") {
+        url += "&count=" + count;
     }
-    if(pattern && pattern != "") {
-        url += "&pattern="+pattern;
+    if (pattern && pattern != "") {
+        url += "&pattern=" + pattern;
     }
     url += getConnection();
-    $.get(url,function(res) {
-        if(res && res.cursor) {
+
+    //Send request to server
+    $.get(url, function(res) {
+
+        //If there is a cursor returned
+        if (res && res.cursor) {
+
+            //save the cursor
             cursor = res.cursor;
-            if(res.keys) {
-                res.keys.forEach(function (key) {
-                    $("#keys-output").append("<tr class='keys-row'><td>"+key+"</td></tr>");
+
+            //if there are keys returned
+            if (res.keys) {
+
+                //Iterate all keys and append them to the output table
+                res.keys.forEach(function(key) {
+                    $("#keys-output").append("<tr class='keys-row'><td>" + key + "</td></tr>");
                 });
+
+                //If a key gets clicked
                 $(".keys-row").click(function(ev) {
+
+                    //clear the delete status
                     $("#keys-value-status").html("");
+
+                    //Save the selected row
                     selectedRow = $(ev.currentTarget);
+
+                    //Save the selected key
                     curkey = selectedRow.children().first().html();
-                    var url = "/cli?command=GET "+curkey+getConnection();
-                    $.get(url,function (response) {
+
+                    //Build url and send get request to server
+                    var url = "/cli?command=GET " + curkey + getConnection();
+                    $.get(url, function(response) {
                         try {
+
+                            //If returned value is a json, pretty print to UI
                             var json = JSON.parse(response);
-                            $("#keys-value-value").html(JSON.stringify(json,null,2));
-                        }catch(e){
+                            $("#keys-value-value").html(JSON.stringify(json, null, 2));
+                        } catch (e) {
+
+                            //If returned values is not a json, just display it normally
                             $("#keys-value-value").html(response);
                         }
                     });
--- src/main/resources/public/js/hestia.js
+++ src/main/resources/public/js/hestia.js
@@ -1,30 +1,52 @@
-$(document).ready(function () {
+$(document).ready(function() {
     readServers();
     setupEvents();
 });
 
-
+/**
+ * Read servers from servers.js to display in dropdown and autofill host and port
+ */
 function readServers() {
     var selectlist = $("#connection-server");
-    servers.forEach(function (server) {
-        selectlist.append("<option>"+server.name+"</option>")
+    servers.forEach(function(server) {
+        selectlist.append("<option>" + server.name + "</option>")
     });
 }
 
+/**
+ * setup change events
+ */
 function setupEvents() {
-    $("#cli-command").keydown(function (ev) {
-        if(ev.originalEvent.keyCode == 13) {
+
+    //On cli keypress
+    $("#cli-command").keydown(function(ev) {
+
+        //If keypress is "ENTER"
+        if (ev.originalEvent.keyCode == 13) {
+
+            //Get command from testfield
             var command = $("#cli-command").val();
-            $.get("/cli?command="+command+getConnection(),function (res) {
+
+            //Send Request to server
+            $.get("/cli?command=" + command + getConnection(), function(res) {
+
+                //Display result
                 $("#cli-output").html(res);
             });
         }
     });
 
-    $("#connection-server").change(function () {
+    //On Server dropdown value change
+    $("#connection-server").change(function() {
+
+        //Get selected server
         var selected = $("#connection-server").val();
-        servers.forEach(function (server) {
-            if(server.name == selected) {
+
+        //iterate servers
+        servers.forEach(function(server) {
+
+            //set hostname and port of selected server
+            if (server.name == selected) {
                 $("#connection-host").val(server.host);
                 $("#connection-port").val(server.port);
             }
@@ -32,44 +54,63 @@ function setupEvents() {
 
     });
 
-    $("#connection-database").click(function () {
+    //On Database dropdown click
+    $("#connection-database").click(function() {
+
+        //delete all choices except the first one (Default)
         $("#connection-database").find("option").slice(1).remove();
+
+        //Get host and port
         var host = $("#connection-host").val();
         var port = $("#connection-port").val();
+
+        //Build url
         var url = "/keyspaces";
-        if(host && port) {
-            url+="?host="+host+"&port="+port;
+        if (host && port) {
+            url += "?host=" + host + "&port=" + port;
         }
-        $.get(url, function (res) {
-            if(res.length > 0) {
+
+        //Send request to server
+        $.get(url, function(res) {
+
+            //if there are databases
+            if (res.length > 0) {
+
+                //Fill in the options
                 var selectlist = $("#connection-database");
-                res.forEach(function (keyspace) {
-                    selectlist.append("<option>"+keyspace+"</option>");
+                res.forEach(function(keyspace) {
+                    selectlist.append("<option>" + keyspace + "</option>");
                 });
             }
         });
     });
 }
 
+/**
+ * Get hostname, port and database from respective input forms and parse them into an url
+ * @returns {string}
+ */
 function getConnection() {
     var host = $("#connection-host").val();
     var port = $("#connection-port").val();
     var db = $("#connection-database").val();
-    if(db.startsWith("Default")) {
+
+    //Get Database integer
+    if (db.startsWith("Default")) {
         db = 0;
     } else {
         db = db.split(":")[0];
         db = db.substr(2, db.length);
     }
     var url = "";
-    if(host) {
-        url += "&host="+host;
+    if (host) {
+        url += "&host=" + host;
     }
-    if(port) {
-        url += "&port="+port;
+    if (port) {
+        url += "&port=" + port;
     }
-    if(db) {
-        url += "&db="+db;
+    if (db) {
+        url += "&db=" + db;
     }
     return url;
 }
